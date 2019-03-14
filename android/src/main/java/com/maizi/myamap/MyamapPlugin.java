2f25f9b4d9fb9194797a4559b6c7cacc190a8163
mymaizi <lqjlmp@gmail.com>
2019-03-14 12:15:09 +0800

    Flutter高德地图插件

---
+++ android/src/main/java/com/maizi/myamap/MyamapPlugin.java
@@ -0,0 +1,57 @@
+package com.maizi.myamap;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+import io.flutter.plugin.common.MessageCodec;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.StandardMessageCodec;
+import io.flutter.plugin.platform.PlatformView;
+import io.flutter.plugin.platform.PlatformViewFactory;
+
+/** MyamapPlugin */
+public class MyamapPlugin {
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar, Bundle savedInstanceState) {
+        MapView _mapView = new MapView(registrar.activity());
+        _mapView.onCreate(savedInstanceState);
+        registrar.platformViewRegistry().registerViewFactory("MyAmapView", new MyAmapViewFactory(_mapView, new StandardMessageCodec()));
+    }
+
+    static class MyAmapViewFactory extends PlatformViewFactory {
+        MapView _mapView;
+
+        MyAmapViewFactory(MapView mapView, MessageCodec<Object> messageCodec) {
+            super(messageCodec);
+            _mapView = mapView;
+        }
+
+        @Override
+        public PlatformView create(Context context, int i, Object o) {
+            return new PlatformView() {
+                @Override
+                public View getView() {
+                    _mapView.getMap();
+                    return _mapView;
+                }
+
+                @Override
+                public void dispose() {
+
+                }
+            };
+        }
+    }
+}
