d891feff6736e8af099c67e8837dcea8f6c58108
mymaizi <lqjlmp@gmail.com>
2019-03-18 13:52:24 +0800

    新增获取定位地址

---
+++ example/android/app/src/main/java/com/maizi/myamap/MainActivity.java
@@ -0,0 +1,17 @@
+package com.maizi.myamap;
+
+import android.os.Bundle;
+
+import com.maizi.myamap.MyamapPlugin;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        MyamapPlugin.registerWith(savedInstanceState);
+    }
+}
